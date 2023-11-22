88dcd588a56bb91128c8c9d1cef23e68805406af
olivabaeriswyl <122967424+olivabaeriswyl@users.noreply.github.com>
2023-11-22 14:21:15 +0100

    burger menu, animations

--- js/main.js
+++ js/main.js
@@ -5,4 +5,14 @@ var toggleMenu = function toggleMenu() {
   menuburger.classList.toggle('is-active');
 }
 
+var closeMenu = function closeMenu() {
+  var width = window.innerWidth
+  if(width > 768){
+    menuburger.classList.remove('is-active');
+  }
+}
+
 burger.addEventListener('click', toggleMenu);
+
+window.addEventListener('resize', closeMenu)
+
